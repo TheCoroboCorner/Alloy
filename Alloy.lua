72f8ebde08bf3c406870787fb6d0ea5c6749adb3
Corobo <81382526+TheCoroboCorner@users.noreply.github.com>
2026-05-04 06:24:37 -0400

    Integrate CoroboUtils

--- Alloy.lua
+++ Alloy.lua
@@ -6,25 +6,80 @@ ALLOY.shield_colour_dull = HEX('164E75')
 ALLOY.shield_colour_bright = HEX('4CDFFC')
 ALLOY.shield_colour_white = HEX('99EEFF')
 
--- default_health_min is the lowest value HP can be before game over triggers
-ALLOY.default_health_min = 0
--- default_health_max is the highest value HP can be
-ALLOY.default_health_max = 100
--- starting_health is the starting HP value
-ALLOY.starting_health = ALLOY.default_health_max
+-- ALLOY.default = {}
+-- ALLOY.modifiers = {}
+-- ALLOY.actions = {}
+-- ALLOY.debug = {}
 
--- default_shield_min is the lowest value shield can be before depleting HP
-ALLOY.default_shield_min = 0
--- default_shield_max is the highest value shield can be
-ALLOY.default_shield_max = 25
+CUTIL.add_variable("alloy_health", 100)
+CUTIL.add_variable("alloy_shield", 0)
+
+-- default.health_min is the lowest value HP can be before game over triggers
+--ALLOY.default.health_min = 0
+CUTIL.add_variable("alloy_health_min", 0)
+
+-- default.health_max is the highest value HP can be
+-- ALLOY.default.health_max = 100
+CUTIL.add_variable("alloy_health_max", 100)
+
+-- starting_health is the starting HP value
+-- ALLOY.starting_health = ALLOY.default.health_max
+CUTIL.add_variable("alloy_starting_health", 100)
+
+-- default.shield_min is the lowest value shield can be before depleting HP
+-- ALLOY.default.shield_min = 0
+CUTIL.add_variable("alloy_shield_min", 0)
+
+-- default.shield_max is the highest value shield can be
+-- ALLOY.default.shield_max = 25
+CUTIL.add_variable("alloy_shield_max", 25)
+
+-- default.shield_bonus is the bonus given at the end of a round where only one hand was played
+-- ALLOY.default.shield_bonus = 5
+CUTIL.add_variable("alloy_shield_bonus", 5)
+
+-- default.explosive_fuse_rounds is how many rounds it takes before explosive cards blow up
+-- ALLOY.default.explosive_fuse_rounds = 6
+CUTIL.add_variable("alloy_explosive_fuse_rounds", 6)
+
 -- starting_shield is the starting shield value
-ALLOY.starting_shield = ALLOY.default_shield_min
+-- ALLOY.starting_shield = ALLOY.default.shield_min
+CUTIL.add_variable("alloy_starting_shield", 0)
+
+-- Modifiers (for use in Stakes and whatnot
+-- ALLOY.modifiers.health_min = 0
+-- ALLOY.modifiers.health_max = 0
+-- ALLOY.modifiers.starting_health = 0
+
+-- ALLOY.modifiers.shield_min = 0
+-- ALLOY.modifiers.shield_max = 0
+-- ALLOY.modifiers.starting_shield = 0
+-- ALLOY.modifiers.shield_bonus = 0
+
+-- ALLOY.modifiers.explosive_fuse_rounds = 0
+
+-- These are Actions. You use ALLOY.add_modifier to use these
+-- ALLOY.actions.health_drain = {}
+-- ALLOY.actions.shield_drain = {}
+-- ALLOY.actions.dot = {}
+
+-- Debug protocols
+ALLOY.debug = {}
+ALLOY.debug.unlock_all_stakes = true
 
 to_number = to_number or function(x) return x end
+
+ALLOY.game_end = function(win)
+	if win then CUTIL.game_win()
+	else CUTIL.game_lose()
+	end
+end
 
 assert(SMODS.load_file("src/hold.lua"))()
 assert(SMODS.load_file("src/lock.lua"))()
 assert(SMODS.load_file("src/health.lua"))()
+assert(SMODS.load_file("src/stakes.lua"))()
+assert(SMODS.load_file("src/stickers.lua"))()
 
 --[[
 
