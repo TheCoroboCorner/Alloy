86b095c36c280439b62cad92f70cd08103a9d16d
Corobo <81382526+TheCoroboCorner@users.noreply.github.com>
2026-05-04 06:26:10 -0400

    Update localization

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1,4 +1,104 @@
 return {
+	descriptions = {
+		Stake = {
+			stake_alloy_blood = {
+				name = "Blood Stake",
+				text = {
+					"Maximum health is",
+					"decreased by {C:attention}50",
+					"{s:0.8}Applies all previous Stakes"
+				}
+			},
+			stake_alloy_tall = {
+				name = "Tall Stake",
+				text = {
+					"Required score scales",
+					"much faster for each {C:attention}Ante",
+					"{s:0.8}Applies all previous Stakes"
+				}
+			},
+			stake_alloy_wood = {
+				name = "Wooden Stake",
+				text = {
+					"Shop can have {C:attention}Vampiric{} Jokers",
+					"{C:inactive,s:0.8}(Attacks you for one damage at the",
+					"{C:inactive,s:0.8}end of round, ignores shield)",
+					"{s:0.8}Applies all previous Stakes"
+				}
+			},
+			stake_alloy_jammed = {
+				name = "Jammed Stake",
+				text = {
+					"{C:red}-1{} Hand",
+					"{s:0.8}Applies all previous Stakes"
+				}
+			},
+			stake_alloy_mountain = {
+				name = "Mountain Stake",
+				text = {
+					"Required score scales",
+					"a lot faster for each {C:attention}Ante",
+					"{s:0.8}Applies all previous Stakes"
+				}
+			},
+			stake_alloy_unstable = {
+				name = "Unstable Stake",
+				text = {
+					"Shop can have {C:attention}Explosive{} Jokers",
+					"{C:inactive,s:0.8}(Explodes after {C:attention,s:0.8}6 {C:inactive,s:0.8}rounds, taking itself and",
+					"{C:inactive,s:0.8}and adjacent Jokers out, ignoring Eternal)",
+					"{s:0.8}Applies all previous Stakes"
+				}
+			},
+			stake_alloy_platinum = {
+				name = "Platinum Stake",
+				text = {
+					"Take {C:attention}5{} damage at",
+					"the end of each round",
+					"{s:0.8}Applies all previous Stakes"
+				}
+			},
+			stake_alloy_warrior = {
+				name = "Warrior Stake",
+				text = {
+					"Skipping a blind now",
+					"makes you take {C:attention}10{} damage",
+					"{s:0.8}Applies all previous Stakes"
+				}
+			},
+			stake_alloy_champion = {
+				name = "Champion Stake",
+				text = {
+					"Required score scales",
+					"{C:attention}drastically{} for each {C:attention}Ante",
+					"{s:0.8}Applies all previous Stakes"
+				}
+			},
+			stake_alloy_legend = {
+				name = "Legend Stake",
+				text = {
+					"{C:legendary,E:1}Beat four more Antes to win",
+					"{s:0.8}Applies all previous Stakes"
+				}
+			}
+		},
+		Other = {
+			alloy_vampire = {
+				name = "Vampire Sticker",
+				text = {
+					"Lose {C:red}#1#{} HP at end of round"
+				}
+			},
+			alloy_explosive = {
+				name = "Explosive Sticker",
+				text = {
+					"{C:red}Explodes{} in #1# rounds and",
+					"{C:red}destroys{} all adjacent Jokers,",
+					"{C:attention}ignores Eternal"
+				}
+			}
+		}
+	},
 	misc = {
 		dictionary = {
 			ph_health = "But it refused.",
@@ -9,6 +109,10 @@ return {
 			
 			b_locked = "Locked ",
 			b_unlocked = "Unlocked ",
+		},
+		labels = {
+			alloy_vampire = "Vampire",
+			alloy_explosive = "Explosive"
 		}
 	}
 }
