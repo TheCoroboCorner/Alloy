5499c1fdc3fcaed4df07fd7ab1c9e51a65a99c76
Corobo <81382526+TheCoroboCorner@users.noreply.github.com>
2026-05-06 03:18:54 -0400

    Create defs

---
+++ src/defs/stakes.lua
@@ -0,0 +1,314 @@
+local function apply_blood_stake_health_max(health_max, params)
+	return G.GAME.blood_stake_active and (health_max - params.penalty) or health_max
+end
+
+local function apply_blood_stake_starting_health(starting_health, params)
+	return G.GAME.blood_stake_active and (starting_health - params.penalty) or starting_health
+end
+
+local function apply_platinum_stake_dot(self, card, context)
+	if G.GAME.platinum_stake_active then
+		ALLOY.ease_damage(-5)
+	end
+end
+
+local function apply_warrior_stake_damage_on_skip(self, card, context)
+	if G.GAME.warrior_stake_active then
+		print("test")
+		ALLOY.ease_damage(-10)
+	end
+end
+
+CUTIL.register_modifier_type("blood_stake_health_max", apply_blood_stake_health_max)
+CUTIL.register_modifier_type("blood_stake_starting_health", apply_blood_stake_starting_health)
+
+SMODS.Stake {
+	name = "Blood Stake",
+	key = "blood",
+	
+	unlocked = ALLOY.debug.unlock_all_stakes,
+	unlocked_stake = "tall",
+	applied_stakes = { "gold" },
+	prefix_config = { applied_stakes = { mod = false } },
+	
+	atlas = "alloy_stakes",
+	pos = {
+		x = 0,
+		y = 0
+	},
+	sticker_atlas = "alloy_stickers",
+	sticker_pos = {
+		x = 0,
+		y = 1
+	},
+
+
+	modifiers = function()
+		G.GAME.blood_stake_active = true
+		
+		CUTIL.add_modifier("alloy_health_max", "blood_stake_health_max", { penalty = 50 }, 1)
+		CUTIL.add_modifier("alloy_starting_health", "blood_stake_starting_health", { penalty = 50 }, 1)
+	end,
+	
+	colour = G.C.SUITS.Hearts,
+	loc_txt = {}
+}
+
+SMODS.Stake {
+	name = "Tall Stake",
+	key = "tall",
+		
+	unlocked = ALLOY.debug.unlock_all_stakes,
+	unlocked_stake = "wood",
+	applied_stakes = { "blood" },
+	
+	atlas = "alloy_stakes",
+	pos = {
+		x = 1,
+		y = 0
+	},
+	sticker_atlas = "alloy_stickers",
+	sticker_pos = {
+		x = 1,
+		y = 1
+	},
+
+
+	modifiers = function()
+		G.GAME.tall_stake_active = true
+		G.GAME.modifiers.scaling = (G.GAME.modifiers.scaling or 1) + 1
+	end,
+	
+	colour = G.C.SUITS.Spades,
+	loc_txt = {}
+}
+
+SMODS.Stake {
+	name = "Wooden Stake",
+	key = "wood",
+		
+	unlocked = ALLOY.debug.unlock_all_stakes,
+	unlocked_stake = "jammed",
+	applied_stakes = { "tall" },
+	
+	atlas = "alloy_stakes",
+	pos = {
+		x = 2,
+		y = 0
+	},
+	sticker_atlas = "alloy_stickers",
+	sticker_pos = {
+		x = 2,
+		y = 1
+	},
+
+
+	modifiers = function()
+		-- 30% chance for Jokers in shops or booster packs to have a Vampire sticker (take 1 health of damage per round, ignores shield)
+		G.GAME.wood_stake_active = true
+		G.GAME.modifiers.enable_vampire = true
+	end,
+	
+	colour = G.C.UI.TEXT_DARK,
+	loc_txt = {}
+}
+
+SMODS.Stake {
+	name = "Jammed Stake",
+	key = "jammed",
+		
+	unlocked = ALLOY.debug.unlock_all_stakes,
+	unlocked_stake = "mountain",
+	applied_stakes = { "wood" },
+	
+	atlas = "alloy_stakes",
+	pos = {
+		x = 3,
+		y = 0
+	},
+	sticker_atlas = "alloy_stickers",
+	sticker_pos = {
+		x = 3,
+		y = 1
+	},
+
+	modifiers = function()
+		G.GAME.jammed_stake_active = true
+		G.GAME.starting_params.hands = G.GAME.starting_params.hands - 1
+	end,
+	
+	colour = G.C.SECONDARY_SET.Tarot,
+	loc_txt = {}
+}
+
+SMODS.Stake {
+	name = "Mountain Stake",
+	key = "mountain",
+		
+	unlocked = ALLOY.debug.unlock_all_stakes,
+	unlocked_stake = "unstable",
+	applied_stakes = { "jammed" },
+	
+	atlas = "alloy_stakes",
+	pos = {
+		x = 4,
+		y = 0
+	},
+	sticker_atlas = "alloy_stickers",
+	sticker_pos = {
+		x = 4,
+		y = 1
+	},
+
+	modifiers = function()
+		G.GAME.mountain_stake_active = true
+		G.GAME.modifiers.scaling = (G.GAME.modifiers.scaling or 1) + 1
+	end,
+	
+	colour = G.C.SUITS.Diamonds,
+	loc_txt = {}
+}
+
+SMODS.Stake {
+	name = "Unstable Stake",
+	key = "unstable",
+		
+	unlocked = ALLOY.debug.unlock_all_stakes,
+	unlocked_stake = "platinum",
+	applied_stakes = { "mountain" },
+	
+	atlas = "alloy_stakes",
+	pos = {
+		x = 0,
+		y = 1
+	},
+	sticker_atlas = "alloy_stickers",
+	sticker_pos = {
+		x = 0,
+		y = 2
+	},
+
+	modifiers = function()
+		-- 30% chance for Jokers in shops or booster packs to have an Explosive sticker (explodes in 6 rounds and takes out all adjacent Jokers with it, ignoring Eternal)
+		G.GAME.unstable_stake_active = true
+		G.GAME.modifiers.enable_explosive = true
+	end,
+	
+	colour = G.C.GREEN,
+	loc_txt = {}
+}
+
+SMODS.Stake {
+	name = "Platinum Stake",
+	key = "platinum",
+		
+	unlocked = ALLOY.debug.unlock_all_stakes,
+	unlocked_stake = "warrior",
+	applied_stakes = { "unstable" },
+	
+	atlas = "alloy_stakes",
+	pos = {
+		x = 1,
+		y = 1
+	},
+	sticker_atlas = "alloy_stickers",
+	sticker_pos = {
+		x = 1,
+		y = 2
+	},
+
+	modifiers = function()
+		G.GAME.platinum_stake_active = true
+		
+		CUTIL.Events.add("Alloy", "platinum_stake", apply_platinum_stake_dot, { filter = { "end_of_round" } })
+	end,
+	
+	colour = G.C.SECONDARY_SET.Enhanced,
+	loc_txt = {}
+}
+
+SMODS.Stake {
+	name = "Warrior Stake",
+	key = "warrior",
+		
+	unlocked = ALLOY.debug.unlock_all_stakes,
+	unlocked_stake = "champion",
+	applied_stakes = { "platinum" },
+	
+	atlas = "alloy_stakes",
+	pos = {
+		x = 2,
+		y = 1
+	},
+	sticker_atlas = "alloy_stickers",
+	sticker_pos = {
+		x = 2,
+		y = 2
+	},
+
+	modifiers = function()
+		G.GAME.warrior_stake_active = true
+		
+		CUTIL.Events.add("Alloy", "warrior_stake", apply_warrior_stake_damage_on_skip, { filter = { "skip_blind" } })
+	end,
+	
+	colour = G.C.MULT,
+	loc_txt = {}
+}
+
+SMODS.Stake {
+	name = "Champion Stake",
+	key = "champion",
+		
+	unlocked = ALLOY.debug.unlock_all_stakes,
+	unlocked_stake = "legend",
+	applied_stakes = { "warrior" },
+	
+	atlas = "alloy_stakes",
+	pos = {
+		x = 3,
+		y = 1
+	},
+	sticker_atlas = "alloy_stickers",
+	sticker_pos = {
+		x = 3,
+		y = 2
+	},
+	shiny = true,
+
+	modifiers = function()
+		G.GAME.champion_stake_active = true
+		G.GAME.modifiers.scaling = (G.GAME.modifiers.scaling or 1) + 2
+	end,
+	
+	colour = G.C.MONEY,
+	loc_txt = {}
+}
+
+SMODS.Stake {
+	name = "Legend Stake",
+	key = "legend",
+		
+	unlocked = ALLOY.debug.unlock_all_stakes,
+	applied_stakes = { "champion" },
+	
+	atlas = "alloy_stakes",
+	pos = {
+		x = 4,
+		y = 1
+	},
+	sticker_atlas = "alloy_stickers",
+	sticker_pos = {
+		x = 4,
+		y = 2
+	},
+	shiny = true,
+
+	modifiers = function()
+		G.GAME.legend_stake_active = true
+		G.GAME.win_ante = G.GAME.win_ante + 4
+	end,
+	
+	colour = G.C.RARITY.Legendary,
+	loc_txt = {}
+}
---
+++ src/defs/stickers.lua
@@ -0,0 +1,108 @@
+SMODS.Sticker {
+	key = "vampire",
+	badge_colour = HEX('911911'), -- heh
+	needs_enable_flag = true,
+	rate = 0.3,
+	
+	sets = {
+		Joker = true
+	},
+	
+	atlas = "alloy_stickers",
+	pos = {
+		x = 0,
+		y = 0
+	},
+	
+	should_apply = function(self, card, center, area, bypass_roll)
+		return G.GAME.modifiers.enable_vampire and SMODS.Sticker.should_apply(self, card, center, area, bypass_roll)
+	end,
+	
+	loc_vars = function(self, info_queue, card)
+		return {
+			vars = {
+				-1 * (-1) -- -1 * vampire_damage; there's no ability table so I have nowhere to put it
+			}
+		}
+	end,
+	
+	calculate = function(self, card, context)
+		if context.end_of_round and context.main_eval then
+			local vampire_damage = -1
+			ALLOY.ease_health(vampire_damage)
+		end
+	end
+}
+
+SMODS.Sticker {
+	key = "explosive",
+	badge_colour = HEX('524D4D'),
+	needs_enable_flag = true,
+	rate = 0.3,
+	
+	sets = {
+		Joker = true
+	},
+	
+	atlas = "alloy_stickers",
+	pos = {
+		x = 1,
+		y = 0
+	},
+	
+	should_apply = function(self, card, center, area, bypass_roll)
+		return G.GAME.modifiers.enable_explosive and SMODS.Sticker.should_apply(self, card, center, area, bypass_roll)
+	end,
+	
+	apply = function(self, card, val)
+		card.ability[self.key] = val
+		if card.ability[self.key] then
+			card.ability.explosive_fuse = (G.GAME.cutil_vars and G.GAME.cutil_vars.alloy_explosive_fuse_rounds) or CUTIL.get_variable("alloy_explosive_fuse_rounds")
+		end
+	end,
+	
+	loc_vars = function(self, info_queue, card)
+		return {
+			vars = {
+				card.ability.explosive_fuse or G.GAME.cutil_vars.alloy_explosive_fuse_rounds
+			}
+		}
+	end,
+	
+	calculate = function(self, card, context)
+		local function destroy_card(c)
+			if c and not c.getting_sliced then
+				c.getting_sliced = true
+				G.GAME.joker_buffer = G.GAME.joker_buffer - 1
+				G.E_MANAGER:add_event(Event({
+					func = function()
+						G.GAME.joker_buffer = 0
+						c:start_dissolve({ HEX('524D4D') }, nil, 1.6)
+						play_sound('slice1', 0.96 + math.random() * 0.08)
+						return true
+					end
+				}))
+			end
+		end
+	
+		if context.end_of_round and context.main_eval then
+			card.ability.explosive_fuse = card.ability.explosive_fuse - 1
+			if card.ability.explosive_fuse == 0 then
+				local my_pos = nil
+				for i = 1, #G.jokers.cards do
+					if G.jokers.cards[i] == card then
+						my_pos = i
+						break
+					end
+				end
+				
+				local left_card = my_pos and G.jokers.cards[my_pos - 1]
+				local right_card = my_pos and G.jokers.cards[my_pos + 1]
+				
+				destroy_card(left_card)
+				destroy_card(right_card)
+				destroy_card(card)				
+			end
+		end
+	end
+}
